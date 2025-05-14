98910fee713636cf7ac872b6fe7d6114ab3ecf70
vitorbretz <vitorbretz28@gmail.com>
2025-05-13 23:46:15 -0300

    att

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
         colors:{
             'cor1': '#000',
             'cor2': '#fff',
-            'cor5': '#de5'  
+            'cor3': '#201229',
+            'cor4': '#7116E9',
+            'cor5': '#97A5E4',
+            'cor6': '#0C032D',
+            'cor7': '#1C7D94',
+            'cor8': '#24A6A6'
+
         }
     },
   },
