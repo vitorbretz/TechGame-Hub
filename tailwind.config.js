b14cf9acb4883d4d34b8a674c0060ab5a0c7aad1
vitorbretz <vitorbretz28@gmail.com>
2025-05-16 16:44:43 -0300

    att

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./*.html"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
-        colors:{
-            'cor1': '#000',
-            'cor2': '#fff',
-            'cor3': '#201229',
-            'cor4': '#7116E9',
-            'cor5': '#97A5E4',
-            'cor6': '#0C032D',
-            'cor7': '#1C7D94',
-            'cor8': '#24A6A6'
-
-        }
+      colors:{
+        cor1: "#000",
+        cor2: "#fff",
+        cor3: "#201229",
+        cor4: "#7116E9",
+        cor5: "#97A5E4",
+        cor6: "#0C032D",
+        cor7: "#1C7D94",
+        cor8: "#24A6A6",
+      }
     },
   },
   plugins: [],
 }
+
