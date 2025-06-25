e319c0197c7c24f2eb6cbd3ebf2b7e00a2db15fa
vitorbretz <vitorbretz28@gmail.com>
2025-06-24 23:15:26 -0300

    feat
    detalhe footer + cursor pointer

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,16 @@ module.exports = {
         cor6: "#0f0f3d",
         cor7: "#d4bcfa",
         cor8: "#14143a",
-      }
+      },
+      keyframes: {
+        colorAnimation: {
+          '0%': { backgroundPosition: '0% 0%' },
+          '100%': { backgroundPosition: '400% 0%' },
+        },
+      },
+      animation: {
+        colorAnimation: 'colorAnimation 15s linear infinite',
+      },
     },
   },
   plugins: [],
